37b0fc59c00cd8e74dc25317d186210869d15906
Giulio Starace <giulio.starace@gmail.com>
2018-11-22 20:34:15 +0000

    start setting custom parameters for HoG, namely here set that gammaCorrection should be on

--- Scripts/params.py
+++ Scripts/params.py
@@ -58,15 +58,30 @@ DATA_CLASS_NAMES = {
     "pedestrian": 1
 }
 
-################################################################################
-# settings for HOG approaches
+#<section>~~~~~~~~~~~~~~~~~~~~~~~~~~~HoG Settings~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+    #<section>~~~~~~~~~~~~~~~~~HoG Descriptor Settings~~~~~~~~~~~~~~~~~~~~~~~~~~
+HOG_DESC_winSize =(64, 128) #window Size
+HOG_DESC_blockSize = (16,16) # block size
+HOG_DESC_blockStride = (8,8) # block stride
+HOG_DESC_cellSize = (8,8) # cell size
+HOG_DESC_nbins = 9 # number of bins
+HOG_DESC_derivAperture = 1 # not documented
+HOG_DESC_winSigma = -1 #gaussian window smoothing parameter
+HOG_DESC_histogramNormType = 0 #equivalent to L2
+HOG_DESC_L2HysThreshold = 0.2 # L2-Hys normalization method shrinkage.
+HOG_DESC_gammaCorrection = True # whether or not to employ gamma correction
+    #</section>
+
+    #<section>~~~~~~~~~~~~~~~~~~~HoG SVM Settings~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 HOG_SVM_PATH = "../Write/"+sys.argv[1]
 
 HOG_SVM_kernel = cv2.ml.SVM_LINEAR  # see opencv manual for other options
 HOG_SVM_max_training_iterations = 500  # stop training after max iterations
 HOG_SVM_DEGREE = 3
-################################################################################
+    #</section>
+
+#</section>
 
 COLORS = [(0, 0, 0),
           (255, 100, 0),
--- Scripts/utils.py
+++ Scripts/utils.py
@@ -21,14 +21,15 @@ import random
 # set these to false to speed up times (i.e. you don't need to see
 # images/patches/prints)
 
-show_additional_process_information = False;
-show_images_as_they_are_loaded = False;
-show_images_as_they_are_sampled = False;
+show_additional_process_information = False
+show_images_as_they_are_loaded = False
+show_images_as_they_are_sampled = False
 
 ################################################################################
 
 # timing information - for training
 # - helper function for timing code execution
+
 
 def get_elapsed_time(start):
     return (cv2.getTickCount() - start) / cv2.getTickFrequency()
@@ -55,6 +56,7 @@ def print_duration(start):
 # for obvious reasons this will break with a very large dataset as you will run
 # out of memory - so an alternative approach may be required in that case
 
+
 def read_all_images(path):
     images_path = [os.path.join(path, f) for f in os.listdir(path)]
     images = []
@@ -62,7 +64,7 @@ def read_all_images(path):
         img = cv2.imread(image_path)
 
         if show_additional_process_information:
-            print("loading file - ", image_path);
+            print("loading file - ", image_path)
 
         images.append(img)
     return images
@@ -70,6 +72,7 @@ def read_all_images(path):
 ################################################################################
 
 # stack array of items as basic Pyton data manipulation
+
 
 def stack_array(arr):
     stacked_arr = np.array([])
@@ -87,8 +90,10 @@ def stack_array(arr):
 # transform between class numbers (i.e. codes) - {0,1,2, ...N} and
 # names {dog,cat cow, ...} - used in training and testing
 
+
 def get_class_number(class_name):
     return params.DATA_CLASS_NAMES.get(class_name, 0)
+
 
 def get_class_name(class_code):
     for name, code in params.DATA_CLASS_NAMES.items():
@@ -100,25 +105,31 @@ def get_class_name(class_code):
 # image data class object that contains the images, descriptors and bag of word
 # histograms
 
+
 class ImageData(object):
     def __init__(self, img):
         self.img = img
         self.class_name = ""
         self.class_number = None
 
-        # use default parameters for construction of HOG
-        # examples of non-default parameter use here:
-        # https://www.programcreek.com/python/example/84776/cv2.HOGDescriptor
-
-        self.hog = cv2.HOGDescriptor(); # default is 64 x 128
+        #initialize HOGDescriptor object with parameters from params.py
+        self.hog = cv2.HOGDescriptor(params.HOG_DESC_winSize,
+                                     params.HOG_DESC_blockSize,
+                                     params.HOG_DESC_blockStride,
+                                     params.HOG_DESC_cellSize,
+                                     params.HOG_DESC_nbins,
+                                     params.HOG_DESC_derivAperture,
+                                     params.HOG_DESC_winSigma,
+                                     params.HOG_DESC_histogramNormType,
+                                     params.HOG_DESC_L2HysThreshold,
+                                     params.HOG_DESC_gammaCorrection)
         self.hog_descriptor = np.array([])
-
 
     def set_class(self, class_name):
         self.class_name = class_name
         self.class_number = get_class_number(self.class_name)
         if show_additional_process_information:
-            print("class name : ", class_name, " - ", self.class_number);
+            print("class name : ", class_name, " - ", self.class_number)
 
     def compute_hog_descriptor(self):
 
@@ -126,7 +137,8 @@ class ImageData(object):
 
         # resizes the image to ensure that all images are the same size.
 
-        img_hog = cv2.resize(self.img, (params.DATA_WINDOW_SIZE[0], params.DATA_WINDOW_SIZE[1]), interpolation = cv2.INTER_AREA)
+        img_hog = cv2.resize(
+            self.img, (params.DATA_WINDOW_SIZE[0], params.DATA_WINDOW_SIZE[1]), interpolation=cv2.INTER_AREA)
 
         # computes hog descriptor utilizing built-in openCV
 
@@ -136,22 +148,24 @@ class ImageData(object):
             self.hog_descriptor = np.array([])
 
         if show_additional_process_information:
-            print("HOG descriptor computed - dimension: ", self.hog_descriptor.shape);
+            print("HOG descriptor computed - dimension: ",
+                  self.hog_descriptor.shape)
 
 ################################################################################
 
 # generates a set of random sample patches from a given image of a specified size
 # with an optional flag just to train from patches centred around the centre of the image
 
-def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
-                            centre_sampling_offset=10, patch_size=(64,128)):
 
-    patches = [];
+def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
+                     centre_sampling_offset=10, patch_size=(64, 128)):
+
+    patches = []
 
     # if no patches specifed just return original image
 
     if (sample_patches_to_generate == 0):
-        return [img];
+        return [img]
 
     # otherwise generate N sub patches
 
@@ -159,9 +173,9 @@ def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
 
         # get all heights and widths
 
-        img_height, img_width, _ = img.shape;
-        patch_height = patch_size[1];
-        patch_width = patch_size[0];
+        img_height, img_width, _ = img.shape
+        patch_height = patch_size[1]
+        patch_width = patch_size[0]
 
         # iterate to find up to N patches (0 -> N-1)
 
@@ -174,16 +188,20 @@ def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
 
                 # compute a patch location in centred on the centre of the image
 
-                patch_start_h =  math.floor(img_height / 2) - math.floor(patch_height / 2);
-                patch_start_w =  math.floor(img_width / 2) - math.floor(patch_width / 2);
+                patch_start_h = math.floor(
+                    img_height / 2) - math.floor(patch_height / 2)
+                patch_start_w = math.floor(
+                    img_width / 2) - math.floor(patch_width / 2)
 
                 # for the first sample we'll just keep the centre one, for any
                 # others take them from the centre position +/- centre_sampling_offset
                 # in both height and width position
 
                 if (patch_count > 0):
-                    patch_start_h =  random.randint(patch_start_h - centre_sampling_offset, patch_start_h + centre_sampling_offset);
-                    patch_start_w =  random.randint(patch_start_w - centre_sampling_offset, patch_start_w + centre_sampling_offset);
+                    patch_start_h = random.randint(
+                        patch_start_h - centre_sampling_offset, patch_start_h + centre_sampling_offset)
+                    patch_start_w = random.randint(
+                        patch_start_w - centre_sampling_offset, patch_start_w + centre_sampling_offset)
 
                 # print("centred weighted path")
 
@@ -195,22 +213,23 @@ def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
 
                 # randomly select a patch, ensuring we stay inside the image
 
-                patch_start_h =  random.randint(0, (img_height - patch_height));
-                patch_start_w =  random.randint(0, (img_width - patch_width));
+                patch_start_h = random.randint(0, (img_height - patch_height))
+                patch_start_w = random.randint(0, (img_width - patch_width))
 
             # add the patch to the list of patches
 
-            patch = img[patch_start_h:patch_start_h + patch_height, patch_start_w:patch_start_w + patch_width]
+            patch = img[patch_start_h:patch_start_h + patch_height,
+                        patch_start_w:patch_start_w + patch_width]
 
             # show image patches
 
             if (show_images_as_they_are_sampled):
-                cv2.imshow("patch", patch);
-                cv2.waitKey(5);
+                cv2.imshow("patch", patch)
+                cv2.waitKey(5)
 
-            patches.insert(patch_count, patch);
+            patches.insert(patch_count, patch)
 
-        return patches;
+        return patches
 
 ################################################################################
 
@@ -218,7 +237,8 @@ def generate_patches(img, sample_patches_to_generate=0, centre_weighted=False,
 # and optionally adding up to N samples of a specified size with flags for taking them
 # from the centre of the image only with +/- offset in pixels
 
-def load_image_path(path, class_name, imgs_data, samples=0, centre_weighting=False, centre_sampling_offset=10 ,patch_size=(64,128)):
+
+def load_image_path(path, class_name, imgs_data, samples=0, centre_weighting=False, centre_sampling_offset=10, patch_size=(64, 128)):
 
     # read all images at location
 
@@ -228,7 +248,7 @@ def load_image_path(path, class_name, imgs_data, samples=0, centre_weighting=Fal
     for img in imgs:
 
         if (show_images_as_they_are_loaded):
-            cv2.imshow("example", img);
+            cv2.imshow("example", img)
             cv2.waitKey(5)
 
         # generate up to N sample patches for each sample image
@@ -238,8 +258,10 @@ def load_image_path(path, class_name, imgs_data, samples=0, centre_weighting=Fal
         for img_patch in generate_patches(img, samples, centre_weighting, centre_sampling_offset, patch_size):
 
             if show_additional_process_information:
-                print("path: ", path, "class_name: ", class_name, "patch #: ", img_count)
-                print("patch: ", patch_size, "from centre: ", centre_weighting, "with offset: ", centre_sampling_offset)
+                print("path: ", path, "class_name: ",
+                      class_name, "patch #: ", img_count)
+                print("patch: ", patch_size, "from centre: ",
+                      centre_weighting, "with offset: ", centre_sampling_offset)
 
             # add each image patch to the data set
 
@@ -254,14 +276,16 @@ def load_image_path(path, class_name, imgs_data, samples=0, centre_weighting=Fal
 
 # load image data from specified paths
 
-def load_images(paths, class_names, sample_set_sizes, use_centre_weighting_flags, centre_sampling_offset=10, patch_size=(64,128)):
+
+def load_images(paths, class_names, sample_set_sizes, use_centre_weighting_flags, centre_sampling_offset=10, patch_size=(64, 128)):
     imgs_data = []  # type: list[ImageData]
 
     # for each specified path and corresponding class_name and required number
     # of samples - add them to the data set
 
     for path, class_name, sample_count, centre_weighting in zip(paths, class_names, sample_set_sizes, use_centre_weighting_flags):
-        load_image_path(path, class_name, imgs_data, sample_count, centre_weighting, centre_sampling_offset, patch_size)
+        load_image_path(path, class_name, imgs_data, sample_count,
+                        centre_weighting, centre_sampling_offset, patch_size)
 
     return imgs_data
 
@@ -272,12 +296,14 @@ def load_images(paths, class_names, sample_set_sizes, use_centre_weighting_flags
 
 def get_hog_descriptors(imgs_data):
 
-    samples = stack_array([[img_data.hog_descriptor] for img_data in imgs_data])
+    samples = stack_array([[img_data.hog_descriptor]
+                           for img_data in imgs_data])
     return np.float32(samples)
 
 ################################################################################
 
 # return global the set of numerical class labels for the data set of images
+
 
 def get_class_labels(imgs_data):
     class_labels = [img_data.class_number for img_data in imgs_data]
