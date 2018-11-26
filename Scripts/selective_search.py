c6ec3e4b7472885bd0fa0f95beebe0c3a97b7a1d
Giulio Starace <giulio.starace@gmail.com>
2018-11-26 00:00:04 +0000

    eliminate non-max-suppresion bit

--- Scripts/selective_search.py
+++ Scripts/selective_search.py
@@ -33,23 +33,4 @@ def perform_selective_search(image, ss_object, max_rects, min_area):
     # ignoring overly small regions
     accepted_rects = np.array([rect for rect in rects[:max_rects] if (rect[2] * rect[3] > min_area)])
 
-    # extracting column information
-    x1 = accepted_rects[:, 0]
-    y1 = accepted_rects[:, 1]
-    w = accepted_rects[:, 2]
-    h = accepted_rects[:, 3]
-    # calculating second point
-    x2 = x1 + w
-    y2 = y1 + h
-
-    #rebuilding rects in x1, y1, x2, y2 format
-    accepted_rects = np.transpose(np.array([x1, y1, x2, y2]))
-
-    # clearing images from selective search object
-    ss_object.clearImages()
-
-    # locate indices of boxes to be kept after overlap elimination
-    surviving_indeces = non_max_suppression_fast(np.int32(accepted_rects), 0.4)
-
-    # return non-overlapping boxes (to some threshold)
-    return accepted_rects[surviving_indeces]
+    return accepted_rects
