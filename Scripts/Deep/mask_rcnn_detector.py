2383e16c2bf456baf3a6bad1c64c3512e24fbc8b
Giulio Starace <giulio.starace@gmail.com>
2018-12-01 20:46:02 +0000

    add comments

--- Scripts/Deep/mask_rcnn_detector.py
+++ Scripts/Deep/mask_rcnn_detector.py
@@ -1,7 +1,11 @@
 
-
-
 def mask_rcnn_detect(image, model, class_names):
+    """
+    Inputs:
+    -image: np array representing an image
+    -model: Mask RCNN model object as defined in model.py
+    -class_names: list of class names, with indices corresponding to their codes
+    """
     # Run detection on the image
     results = model.detect([image], verbose=0)
 
@@ -11,11 +15,11 @@ def mask_rcnn_detect(image, model, class_names):
     # extract bounding box info
     rects = r['rois']
     # reshape in format desired by detect_and_range.py
-    rects[:, [1, 0]] = rects[:, [0, 1]] # swap column 0 and column 1
-    rects[:, [3, 2]] = rects[:, [2, 3]] # swap column 2 and column 3
+    rects[:, [1, 0]] = rects[:, [0, 1]]  # swap column 0 and column 1
+    rects[:, [3, 2]] = rects[:, [2, 3]]  # swap column 2 and column 3
 
     # getting class ids
-    class_ids =r['class_ids']
+    class_ids = r['class_ids']
 
     # using class ids to index class_names and get detected classes
     classes = class_names[class_ids]
